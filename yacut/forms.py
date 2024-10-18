f3fa66c7539225529b27d24cff2b4336e8f235d7
Alexandr <akatsuki_sanya@vk.com>
2024-10-18 18:41:13 +0500

    Add a form into template

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import URLField, SubmitField
 from wtforms.validators import DataRequired, Length, URL
 
 from .constants import DATA_REQ_MESS, MAX_LENGTH, MIN_LENGTH
 
 class URLForm(FlaskForm):
-    original_link = StringField(
+    original_link = URLField(
         'Длинная ссылка',
         validators=[
             DataRequired(message=DATA_REQ_MESS),
@@ -13,8 +13,8 @@ class URLForm(FlaskForm):
             URL(),
         ]
     )
-    custom_id = StringField(
+    custom_id = URLField(
         'Ваш вариант короткой ссылки',
         validators=[Length(MIN_LENGTH, MAX_LENGTH),]
     )
-    submit = SubmitField('Добавить')
+    submit = SubmitField('Создать')
--- yacut/views.py
+++ yacut/views.py
@@ -4,32 +4,35 @@ from flask import abort, flash, redirect, render_template, url_for
 
 from . import app, db
 from .forms import URLForm
-import random
-import string
+
 from .models import URLMap
 from . import db
+import hashlib
 
-def get_unique_short_id(length=6):
-    characters = string.ascii_letters + string.digits
-    while True:
-        short_id = ''.join(random.choice(characters) for _ in range(length))
-        if not URLMap.query.filter_by(short=short_id).first():
-            return short_id
+def shorten_url(url):
+    short_hash = hashlib.md5(url.encode()).hexdigest()[:8]
+    return f"http://127.0.0.1:5000/{short_hash}"
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLForm()
+    print(2)
+    print(form.validate_on_submit())
     if form.validate_on_submit():
-        original_link = form.original_link.data
-        custom_id = form.custom_id.data or get_unique_short_id()
-
-        if URLMap.query.filter_by(short=custom_id).first():
-            flash('Предложенный вариант короткой ссылки уже существует.')
-        else:
-            new_url = URLMap(original=original_link, short=custom_id)
-            db.session.add(new_url)
-            db.session.commit()
-            flash(f'Короткая ссылка: {url_for("main.redirect_to_url", short=custom_id, _external=True)}')
+        print(1)
+    else:
+        print(form.errors)
+        print(form.data)
+        # original_link = form.original_link.data
+        # custom_id = form.custom_id.data or shorten_url(original_link)
+        #
+        # if URLMap.query.filter_by(short=custom_id).first():
+        #     flash('Предложенный вариант короткой ссылки уже существует.')
+        # else:
+        #     new_url = URLMap(original=original_link, short=custom_id)
+        #     db.session.add(new_url)
+        #     db.session.commit()
+        #     flash(f'Короткая ссылка: {url_for("main.redirect_to_url", short=custom_id, _external=True)}')
 
     return render_template('index.html', form=form)
 
